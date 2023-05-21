dfa6d76370a4c8621c90f309a74b210820b11a9d
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2023-05-20 21:12:53 -0400

    made it look better git push

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -13,19 +13,20 @@ import {
 
 
 import {useEffect, useState} from "react";
+import { Checkbox, Box, Grid } from "@mui/material";
 
 ChartJS.register(LinearScale, BarElement, CategoryScale, Tooltip, Legend, LineElement, PointElement);
 
 
 
 const colors = [
-  "rgba(255, 99, 132, 0.2)",
-  "rgba(255, 159, 64, 0.2)",
-  "rgba(255, 205, 86, 0.2)",
-  "rgba(75, 192, 192, 0.2)",
-  "rgba(54, 162, 235, 0.2)",
-  "rgba(153, 102, 255, 0.2)",
-  "rgba(201, 203, 207, 0.2)",
+  "rgba(255, 99, 132, 1)",
+  "rgba(255, 159, 64, 1)",
+  "rgba(255, 205, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(201, 203, 207, 1)",
 ]
 
 const LineChart = ({ data, chartName }) => {
@@ -88,27 +89,46 @@ const LineChart = ({ data, chartName }) => {
         display: false, // Hide the legend
       },
     },
+    elements: {
+      point: {
+        pointLabel: ({ y }) => y[y.length - 1].toString(), // Display label on the last point
+        font: {
+          size: 12,
+        },
+        borderWidth: 1,
+        borderColor: "rgba(1, 1, 1, 0.5)",
+        backgroundColor: "white",
+        padding: 4,
+        borderRadius: 4,
+      },
+    },
   };
 
   return (
-    <div>
-      <h3>{chartName}</h3>
-      <div>
+
+    <>
+    <h3>{chartName}</h3>
+    <Grid container>
+      
+    <Grid item xs={2}>
+      
+      <Box sx={{ border: "1px solid black" }}>
         {sortedKeys.map((key, i) => (
-          <button
-            key={key}
-            onClick={() => handleDatasetToggle(key)}
-            style={{
-              marginRight: '10px',
-              backgroundColor: hiddenDatasets.includes(key) ? 'gray' : 'white',
-            }}
-          >
-            {i+1} {key}
-          </button>
+          <Box key={key} sx={{ borderBottom: "1px solid black" }}>
+            <Checkbox onClick={() => handleDatasetToggle(key)} checked={!hiddenDatasets.includes(key)} />
+            <span>{i + 1} {key}</span>
+          </Box>
         ))}
+      </Box>
+    </Grid>
+
+    <Grid item xs={10} sx={{padding: "32px"}}>
+      <div style={{ position: "sticky", top: "50%", transform: "translateY(-50%)" }}>
+        <Line data={chartData} options={options} />
       </div>
-      <Line data={chartData} options={options} />
-    </div>
+    </Grid>
+  </Grid>
+  </>
   );
 
 
